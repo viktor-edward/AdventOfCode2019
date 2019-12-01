e105df86568b7da479564d299fc499561b38d847
Viktor <viktor.edward@visigon.se>
2019-12-01 11:30:06 +0100

    Day 1 challenge completed

--- src/day1.py
+++ src/day1.py
@@ -1,6 +1,44 @@
-def helloWorld():
-    return "Hello World!"
+import math
+
+
+def readFile(fileInput):
+    f = open(fileInput, 'r')
+    data = []
+    for line in f.readlines():
+        data.append(float(line))
+    f.close()
+    return data
+
+
+def calculateFuelRequired(mass):
+    return math.floor(mass/3) - 2
+
+
+def calculateFuelRequiredRecursive(mass):
+    fuelReqCurrent = calculateFuelRequired(mass)
+    if fuelReqCurrent > 0:
+        fuelReq = fuelReqCurrent + calculateFuelRequiredRecursive(fuelReqCurrent)
+        return fuelReq
+    else:
+        return 0
+
 
 
 def main():
-    print(helloWorld())
+    listOfMass = readFile("../resources/day1_input.txt")
+
+    print("Part one: ")
+    listOfFuelRequired = []
+    for mass in listOfMass:
+        listOfFuelRequired.append(calculateFuelRequired(mass))
+    print(sum(listOfFuelRequired))
+
+    print("Part two: ")
+    listOfFuelRequiredRec = []
+    for mass in listOfMass:
+        listOfFuelRequiredRec.append(calculateFuelRequiredRecursive(mass))
+    print(sum(listOfFuelRequiredRec))
+
+
+if __name__ == '__main__':
+    main()
